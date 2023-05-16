ac8b00b20ff29eaa04f262419fc9d16969930b09
leomaden_cam <lm859@cam.ac.uk>
2023-05-16 16:25:38 +0100

    Add fit lines

--- Scripts/ESDU_diffuser_length.py
+++ Scripts/ESDU_diffuser_length.py
@@ -13,16 +13,24 @@ plt.rcParams.update({"font.size": 20})
 
 # Data for area ratio and optimum pressure recovery
 # from EDSU
-sigma = esdu.Cpr2_data_symmetrical["A2/A1"]
+A2_A1 = esdu.Cpr2_data_symmetrical["A2/A1"]
 L_h1 = esdu.Cpr2_data_symmetrical["L/h1"]
+
+poly_fit = np.polynomial.Polynomial.fit(A2_A1, L_h1, deg=2)
+fit_x = np.linspace(1.1, 3.5, 100)
+fit_y = poly_fit(fit_x)
+a, b, c = poly_fit.coef
+fit_str = fr"$ {a:.3g} + {b:.3g}x + {c:.3g}x^2 $"
 
 # Plot results
 plt.figure(figsize=(10, 8))
-plt.plot(sigma, L_h1, marker="x")
+plt.plot(A2_A1, L_h1, marker="x", label="Data")
+plt.plot(fit_x, fit_y, label=f"Fit: {fit_str}")
 
 plt.xlabel(r"$ \dfrac{A_2}{A_1} $")
 plt.ylabel(r"$ \dfrac{L}{h_1} $", rotation=0, labelpad=20)
 plt.grid()
 plt.tight_layout()
+plt.legend()
 plt.savefig("Figures/ESDU_diffuser_length.pdf")
 plt.show()
--- Scripts/ESDU_vs_Ideal_diffuser.py
+++ Scripts/ESDU_vs_Ideal_diffuser.py
@@ -8,22 +8,31 @@ sys.path.append("./Modules")
 import esdu_75026 as esdu
 import numpy as np
 import matplotlib.pyplot as plt
+import scipy.optimize
 
 plt.rcParams.update({"font.size": 20})
 
 # Data for area ratio and optimum pressure recovery
 # from EDSU
-sigma = esdu.Cpr2_data_symmetrical["A2/A1"]
+A2_A1 = esdu.Cpr2_data_symmetrical["A2/A1"]
 Cpr_opt = esdu.Cpr2_data_symmetrical["Cpr"]
 
 # Ideal diffuser using Bernoulli
-sigma_ideal = np.linspace(min(sigma), max(sigma), 100)
-Cpr_opt_ideal = 1 - 1 / sigma_ideal**2
+A2_A1_ideal = np.linspace(min(A2_A1), max(A2_A1), 100)
+Cpr_opt_ideal = 1 - 1 / A2_A1_ideal**2
+
+# ESDU Fit
+fit_func = lambda x, a, b: a - b / x**2
+(a_fit, b_fit), _ = scipy.optimize.curve_fit(fit_func, A2_A1, Cpr_opt)
+fit_x = A2_A1_ideal
+fit_y = fit_func(fit_x, a_fit, b_fit)
+fit_str = r"$" + f"{a_fit:.3g} - {b_fit:.3g}/x^2 $"# + r"(A_2/A_1)^{-2} $"
 
 # Plot results
 plt.figure(figsize=(10, 8))
-plt.plot(sigma_ideal, Cpr_opt_ideal, label="Ideal")
-plt.plot(sigma, Cpr_opt, marker="x", label="ESDU")
+plt.plot(A2_A1_ideal, Cpr_opt_ideal, label="Ideal")
+plt.plot(A2_A1, Cpr_opt, marker="x", label="ESDU")
+plt.plot(fit_x, fit_y, label=f"Fit: {fit_str}")
 
 plt.xlabel(r"$ \dfrac{A_2}{A_1} $")
 plt.ylabel(r"$ C_{pr}^{**} $")
