218695218a3581a70673b7b8197d012c3a1fee0f
leomaden_cam <lm859@cam.ac.uk>
2023-05-17 02:14:58 +0100

    Improve script to analyse data and process runs 1 and 2 and update Python script accordingly

--- Experimental Data/process_data.m
+++ Experimental Data/process_data.m
@@ -1,3 +1,5 @@
+% Sampling rate for calculating shaft frequency
+f_sample = 80000;
 
 filename = "run2";
 
@@ -5,59 +7,101 @@ data = load(filename + ".mat");
 e = data.e;
 N = data.N;
 
-out = e;
-
-out = rmfield(out, "long_force");
-out = rmfield(out, "lat_force");
-out = rmfield(out, "J");
-out = rmfield(out, "calib");
-out = rmfield(out, "rho");
-out = rmfield(out, "env");
-
-% FFT of shaft freq to get average
-
-
-out.raw = rmfield(out.raw, "V_shaft");
-out.raw = rmfield(out.raw, "t");
-out.raw = rmfield(out.raw, "V_long");
-out.raw = rmfield(out.raw, "V_lat");
-out.raw = rmfield(out.raw, "V");
-out.raw = rmfield(out.raw, "pxie_time");
-out.raw = rmfield(out.raw, "V_Long_av");
-out.raw = rmfield(out.raw, "V_Lat_av");
-
-out.raw.V_long_avg = e.raw.V_Long_av;
-out.raw.V_lat_avg = e.raw.V_Lat_av;
-
-out.load_calib = e.calib.load;
-
-
-% Fix run1
+%% Fix run1
 if filename == "run1"
-    out.aoa(end-1:end) = [];
-    out.tunnel_switch(end) = [];
+    e.aoa(end-1:end) = [];
+    e.tunnel_switch(end) = [];
 
-    out.raw.V_long_avg(2:3, :, :) = [];
-    out.raw.V_long_avg(:, end, :) = [];
+    e.raw.V_long(2:3, :, :, :) = [];
+    e.raw.V_long(:, end, :, :) = [];
 
-    out.raw.V_lat_avg(2:3, :, :) = [];
-    out.raw.V_lat_avg(:, end, :) = [];
+    e.raw.V_lat(2:3, :, :, :) = [];
+    e.raw.V_lat(:, end, :, :) = [];
 
-    out.raw.P(2:3, :, :, :) = [];
-    out.raw.P(:, end, :, :) = [];
-    out.raw.P(:, :, :, 3:end) = [];
+    e.raw.P(2:3, :, :, :) = [];
+    e.raw.P(:, end, :, :) = [];
+    e.raw.P(:, :, :, 3:end) = [];
 
-    out.raw.T_motor(2:3, :, :) = [];
-    out.raw.T_motor(:, end, :) = [];
+    e.raw.T_motor(2:3, :, :) = [];
+    e.raw.T_motor(:, end, :) = [];
 
-    out.raw.I_supply(2:3, :, :) = [];
-    out.raw.I_supply(:, end, :) = [];
+    e.raw.I_supply(2:3, :, :) = [];
+    e.raw.I_supply(:, end, :) = [];
 
-    out.raw.shaft_freq(2:3, :, :) = [];
-    out.raw.shaft_freq(:, end, :) = [];
+    e.raw.shaft_freq(2:3, :, :) = [];
+    e.raw.shaft_freq(:, end, :) = [];
 end
 
-e = out;
+%% Extract data from e
+geom = e.geom;
+atm = e.atm;
 
-save(filename + "_proc.mat", "e", "N");
+V_supply = e.raw.V_supply;
+I_supply = e.raw.I_supply;
+P_total = e.raw.P(:, :, :, N.Po_tunnel);
+P_static = e.raw.P(:, :, :, N.Ps_tunnel);
+T_motor = e.raw.T_motor;
+
+aoa = e.aoa;
+tunnel_switch = e.tunnel_switch;
+pwm_speed = e.pwm_speed;
+
+% Calculate shaft frequency
+f_shaft = zeros(length(aoa), length(tunnel_switch), length(pwm_speed));
+
+for i = 1:length(aoa)
+    for j = 1:length(tunnel_switch)
+        for k = 1:length(pwm_speed)
+            V_shaft = squeeze(e.raw.V_shaft(i, j, k, :));
+            t = squeeze(e.raw.t(i, j, k, :));
+            L = length(t);
+
+            % Zero mean
+            V_shaft = V_shaft - mean(V_shaft);
+
+            Y = fft(V_shaft);
+            P2 = abs(Y/L);
+            P1 = P2(1:L/2+1);
+            P1(2:end-1) = 2*P1(2:end-1);
+
+            f = f_sample * (0:(L/2))/L;
+            [~, i_peak] = max(P1);
+            f_shaft(i, j, k) = f(i_peak);
+
+%             figure;
+%             plot(f, P1)
+%             title(['PWM Speed = ' num2str(pwm_speed(k))])
+        end
+    end
+end
+
+% Calculate load cell forces
+load_calib = e.calib.load;
+
+V_long_avg = mean(e.raw.V_long, 4);
+V_lat_avg = mean(e.raw.V_long, 4);
+
+F_long = load_calib(1, 1) * V_long_avg + load_calib(1, 2);
+F_lat = load_calib(2, 1) * V_lat_avg + load_calib(2, 2);
+
+%% Fill new e struct
+e_new.geom = geom;
+e_new.atm = atm;
+
+e_new.V_supply = V_supply;
+e_new.I_supply = I_supply;
+e_new.P_total = P_total;
+e_new.P_static = P_static;
+e_new.T_motor = T_motor;
+e_new.f_shaft = f_shaft;
+e_new.F_long = F_long;
+e_new.F_lat = F_lat;
+
+e_new.aoa = aoa;
+e_new.tunnel_switch = tunnel_switch;
+e_new.pwm_speed = pwm_speed;
+
+%% Save e_new into new file as e
+e = e_new;
+save(filename + "_processed.mat", "e");
 
--- Modules/experimental.py
+++ Modules/experimental.py
@@ -21,66 +21,84 @@ class ExpAtm:
     Ta : float
     Ha : float
 
-@dataclass
-class ExpRaw:
-    V_supply : float
-    P : np.ndarray
-    T_motor : np.ndarray
-    I_supply : np.ndarray
-    shaft_freq : np.ndarray
-    V_long_avg : np.ndarray
-    V_lat_avg : np.ndarray
+# @dataclass
+# class ExpRaw:
+#     V_supply : float
+#     P : np.ndarray
+#     T_motor : np.ndarray
+#     I_supply : np.ndarray
+#     shaft_freq : np.ndarray
+#     V_long_avg : np.ndarray
+#     V_lat_avg : np.ndarray
+
+# @dataclass
+# class ExpData:
+#     geom : ExpGeom
+#     atm : ExpAtm
+#     raw : ExpRaw
+#     aoa : np.ndarray
+#     tunnel_switch : np.ndarray
+#     pwm_speed : np.ndarray
+#     load_calib : np.ndarray
+
+#     def __post_init__(self):
+#         self.geom = ExpGeom(**self.geom)
+#         self.atm = ExpAtm(**self.atm)
+#         self.raw = ExpRaw(**self.raw)
 
 @dataclass
 class ExpData:
     geom : ExpGeom
     atm : ExpAtm
-    raw : ExpRaw
+    
+    V_supply : float
+    I_supply : np.ndarray
+    P_total : np.ndarray
+    P_static : np.ndarray
+    T_motor : np.ndarray
+    f_shaft : np.ndarray
+    F_long : np.ndarray
+    F_lat : np.ndarray
+
     aoa : np.ndarray
     tunnel_switch : np.ndarray
     pwm_speed : np.ndarray
-    load_calib : np.ndarray
 
     def __post_init__(self):
         self.geom = ExpGeom(**self.geom)
         self.atm = ExpAtm(**self.atm)
-        self.raw = ExpRaw(**self.raw)
+
+    @staticmethod
+    def from_dict(data):
+        return ExpData(**data)
 
 
-def parse(s):
-    names = s.dtype.names
+
+def parse_mat(data):
+    names = data.dtype.names
 
     s_dict = {}
 
     for name in names:
-        content = s[name].item()
+        content = data[name].item()
 
         if type(content) == np.ndarray and content.shape == ():
             # Data stored in s.name is a struct; recurse
-            s_dict[name] = parse(content)
+            s_dict[name] = parse_mat(content)
         else:
-            # Content is primitive type of ndarray
+            # Content is primitive type or non empty ndarray
             s_dict[name] = content
 
     return s_dict
 
-def load_data():
-    e_runs = {}
-    N_runs = {}
-    runs = ["run1", "run2"]
-    path = "Experimental Data/{}_proc.mat"
+def load_data(file_paths):
+    e_runs = []
 
-    for run in runs:
-        run_path = path.format(run)
-        data = scipy.io.loadmat(run_path, squeeze_me=True)
+    for i, path in enumerate(file_paths):
+        data = scipy.io.loadmat(path, squeeze_me=True)        
+        
+        e_dict = parse_mat(data["e"])
+        exp_data = ExpData.from_dict(e_dict)
+        e_runs.append(exp_data)
 
-        e_runs[run] = parse(data["e"])
-        N_runs[run] = parse(data["N"])
-
-    return e_runs, N_runs
-
-if __name__ == "__main__":
-    e_runs, N_runs = load_data()
-
-    e = ExpData(**e_runs["run1"])
-    pass
+    return e_runs
