889bfa659117528b76dd1e69a0d7178a24cfb632
leomaden_cam <lm859@cam.ac.uk>
2023-05-24 03:41:30 +0100

    Allow for diffusers of sigma=1 by lerping to zero

--- modules/esdu_75026.py
+++ modules/esdu_75026.py
@@ -3,18 +3,25 @@ import numpy as np
 
 
 def optimum_Cpr2_symmetrical(A2_A1):
+    # Add point at A2/A1 = 1, Cpr = 0, L/h1 = 0
+    new_data = pd.DataFrame.from_dict({"A2/A1": [1], "Cpr": [0], "L/h1": [0]})
+    data = pd.concat([Cpr2_data_symmetrical, new_data]).sort_values("A2/A1")
+
     x = A2_A1
-    xp = Cpr2_data_symmetrical["A2/A1"]
+    xp = data["A2/A1"]
 
-    fp1 = Cpr2_data_symmetrical["Cpr"]
-    fp2 = Cpr2_data_symmetrical["L/h1"]
+    fp1 = data["Cpr"]
+    fp2 = data["L/h1"]
 
-    Cpr2 = np.interp(x, xp, fp1)
-    L_h1 = np.interp(x, xp, fp2)
+    Cpr2 = np.interp(x, xp, fp1, left=0)
+    L_h1 = np.interp(x, xp, fp2, left=0)
 
     return Cpr2, L_h1
 
 def calc_phi(A2_A1, L_h1):
+    if L_h1 == 0:
+        return 0
+
     tan_phi = (A2_A1 - 1) / (2 * L_h1)
     phi = np.rad2deg(np.arctan(tan_phi))
     return phi
