87a5e7dcce7661ecfc99659a8a6859d623b7e311
leomaden_cam <lm859@cam.ac.uk>
2023-05-29 02:20:17 +0100

    Improve optimiser script

--- Optimise_range.py
+++ Optimise_range.py
@@ -8,42 +8,14 @@ import matplotlib.pyplot as plt
 
 
 
+# ------------------------------------------------------------
+#                Optimiser fixed parameters
+# ------------------------------------------------------------
 
-
-
-
-# def calc_phi(J, sigma, alpha, L_D):
-
-#     C_Tx_func = lambda phi: aero_model.calc_C_thrust_x(phi, sigma, alpha, J)
-#     C_Dx = aero_model.calc_C_drag_x(alpha, L_D)
-
-#     func = lambda phi: C_Tx_func(phi) - C_Dx
-
-#     scipy.optimize.root(func,)
-
-
-
-
-# def calc_range(J, C_power, M_bat):
-#     return J / C_power * M_bat
-
-# def calc_M_prop(sigma, geom):
-
-#     # Diffuser params
-#     Cpr, Lh = esdu.optimum_Cpr2_symmetrical(sigma)
-#     angle = esdu.calc_phi(sigma, Lh)
-
-#     # Update geometry
-#     geom.phi_i = angle
-#     geom.phi_o = angle
-#     geom.Lh_diffuser = Lh
-
-#     # Calc mass
-    
 
 
 # Choose initial parameters
-M_payload = 12
+M_payload = 10
 
 # Mass parameters
 rho_air = 1.2
@@ -58,7 +30,7 @@ mass_params = propulsor_mass.NonDimensionalMassParams(
     rotor_density       =   rho_al / rho_air,
     stator_density      =   rho_resin / rho_air,
     cowl_solidity       =   0.1,
-    hub_solidity        =   0.2
+    hub_solidity        =   0.25
 )
 
 # Base geometry
@@ -81,30 +53,17 @@ geom = geometry.NonDimensionalGeometry(
 
 
 # Initial values/fixed values
-# sigma = 1.2
 eta = 0.9
-# alpha = 10
-# J = 0.5
-# FrU = 5
 phi = 0.7
-hub_tip = geom.rr_hub_tip
-L_D = geom.Lh_total / (2 * geom.Rh_cas)
 
-def calc_s(sigma, alpha, FrU):
 
-    # Update geometry for starting sigma
-    Cpr, Lh = esdu.optimum_Cpr2_symmetrical(sigma)
-    angle = esdu.calc_phi(sigma, Lh)
 
-    Cpr = 1 - 1/sigma**2
-    Lh *= 0.2
+# ------------------------------------------------------------
 
-    geom.phi_i = angle
-    geom.phi_o = angle
-    geom.Lh_diffuser = Lh
 
-    # Update mass model
-    prop_mass_model = propulsor_mass.NonDimensionalMassModel(mass_params, geom)
+
+def calc_aero(phi, sigma, alpha, Cpr, eta, L_D, hub_tip):
+    vals = {}
 
     # Find J from x equation
     C_Tx_func = lambda J: aero_model.calc_C_thrust_x(phi, sigma, alpha, J)
@@ -112,55 +71,123 @@ def calc_s(sigma, alpha, FrU):
     f = lambda J: C_Tx_func(J) - C_Dx * J**2/2
 
     sol = scipy.optimize.root_scalar(f, bracket=(0, 20))
-    # sol = scipy.optimize.root_scalar(f, x0=1, method='newton', fprime=fprime)
     J = sol.root
 
     # Find other variables
+    C_Tx = C_Tx_func(J)
     psi = aero_model.calc_psi(phi, Cpr, J, eta)
-    C_Wdotx = max(aero_model.calc_C_power(phi, psi), 0.06)
+    C_Wdotx = aero_model.calc_C_power(phi, psi)
     C_Ty = aero_model.calc_C_thrust_y(phi, sigma, alpha, J)
     C_Dy = aero_model.calc_C_drag_y(alpha)
 
+    vals["J"] = J
+    vals["C_Tx"] = C_Tx
+    vals["C_Ty"] = C_Ty
+    vals["C_Dx"] = C_Dx
+    vals["C_Dy"] = C_Dy
+    vals["psi"] = psi
+    vals["C_Wdotx"] = C_Wdotx
+
+    return vals
+
+def calc_new_geom(sigma, geom):
+
+    # Update geometry for starting sigma
+    Cpr, Lh = esdu.optimum_Cpr2_symmetrical(sigma)
+
+    # Manipulate Cpr or L/h
+    Cpr = 1 * (1 - 1/sigma**2)
+    Lh *= 0.3
+
+    # Calc new geometry
+    angle = esdu.calc_phi(sigma, Lh)
+    geom.phi_i = angle
+    geom.phi_o = angle
+    geom.Lh_diffuser = Lh
+
+    return Cpr, Lh
+
+def calc_masses(C_Ty, C_Dy, J, FrU, prop_mass_model):
     M_total = (C_Ty - C_Dy * J**2 / 2) * FrU**2
-
     M_prop = prop_mass_model.calc_M_total()
+    M_bat = (M_total - M_prop - M_payload)
 
-    s = J / C_Wdotx * (M_total - M_prop - M_payload)
+    return M_prop, M_bat, M_total
 
+
+hub_tip = geom.rr_hub_tip
+L_D = geom.Lh_total / (2 * geom.Rh_cas)
+
+sigma_conv = []
+alpha_conv = []
+FrU_conv = []
+s_conv = []
+
+def calc_s(sigma, alpha, FrU):
+
+    sigma_conv.append(sigma)
+    alpha_conv.append(alpha)
+    FrU_conv.append(FrU)
+
+    # Update geometry
+    Cpr, Lh = calc_new_geom(sigma, geom)
+
+    # Update mass model
+    prop_mass_model = propulsor_mass.NonDimensionalMassModel(mass_params, geom)
+
+    # Calculate model
+    vals = calc_aero(phi, sigma, alpha, Cpr, eta, L_D, hub_tip)
+
+    C_Ty = vals["C_Ty"]
+    C_Dy = vals["C_Dy"]
+    C_Wdotx = vals["C_Wdotx"]
+    J = vals["J"]
+
+    # Calculate mass
+    M_prop, M_bat, M_total = calc_masses(C_Ty, C_Dy, J, FrU, prop_mass_model)
+
+    # Calculate range
+    s = J / C_Wdotx * M_bat
+
+    s_conv.append(s)
     return s
 
 
 min_func = lambda x: -calc_s(*x)
 
 # sigma, alpha, FrU
-x0 = [1.2, 10, 5]
+x0 = [1.2, 10, 30]
 bounds = [(1, 3), (0, 50), (0, 60)]
 
 # res = scipy.optimize.minimize(min_func, x0, method="Nelder-Mead", bounds=bounds)
-res = scipy.optimize.minimize(min_func, x0, bounds=bounds)
-
-print(-res.fun)
-print(res.x)
+res = scipy.optimize.minimize(min_func, x0, method='L-BFGS-B', bounds=bounds)
 
 sigma, alpha, FrU = res.x
 
-# Find J from x equation
-C_Tx_func = lambda J: aero_model.calc_C_thrust_x(phi, sigma, alpha, J)
-C_Dx = aero_model.calc_C_drag_x(alpha, L_D, hub_tip)
-f = lambda J: C_Tx_func(J) - C_Dx * J**2/2
-
-sol = scipy.optimize.root_scalar(f, bracket=(0, 20))
-J = sol.root
-
-print(f"J = {J}")
+print(res.message)
+print("s = {:.1f}".format(-res.fun))
+print(f"sigma = {sigma:.3g}, alpha = {alpha:.3g}, FrU = {FrU:.3g}")
 
 
-# sigma = np.linspace(1, 2, 50)
-# FrU = 60
-# alpha = 10
-# s = np.zeros_like(sigma)
-# for i, sig in enumerate(sigma):
-#     s[i] = calc_s(sig, alpha, FrU)
+# Print aero values at end
+Cpr, Lh = calc_new_geom(sigma, geom)
+prop_mass_model = propulsor_mass.NonDimensionalMassModel(mass_params, geom)
+vals = calc_aero(phi, sigma, alpha, Cpr, eta, L_D, hub_tip)
+M_prop, M_bat, M_total = calc_masses(vals["C_Ty"], vals["C_Dy"], vals["J"], FrU, prop_mass_model)
 
-# plt.plot(sigma, s)
-# plt.show()
+for key, val in vals.items():
+    print(f"{key} = {val:.3g}")
+
+print(f"Cpr = {Cpr:.3g}")
+print(f"Ld/hr = {Lh:.3g}")
+print(f"M_payload = {M_payload:.3g}")
+print(f"M_prop = {M_prop:.3g}")
+print(f"M_bat = {M_bat:.3g}")
+print(f"M_total = {M_total:.3g}")
+
+plt.plot(sigma_conv/sigma_conv[-1])
+plt.plot(alpha_conv/alpha_conv[-1])
+plt.plot(FrU_conv/FrU_conv[-1])
+plt.plot(s_conv/s_conv[-1])
+plt.grid()
+plt.show()
